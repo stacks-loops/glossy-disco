f37e992db0b7448df3c938855799cbf894b607d9
stacks-loops <p.cronin.connolly@gmail.com>
2024-05-01 18:53:41 -0400

    missed a goose print for this

--- frontend/my-react-app/src/DinoForm.js
+++ frontend/my-react-app/src/DinoForm.js
@@ -23,12 +23,38 @@ function DinoForm() {
         initialValues={initialFormData}
         onSubmit={(values) => console.log(values)}
         >
-        {({ values, setFieldValue }) => (
+        {({ values }) => (
             <Form>
                 <Field type="text" name="title" placeholder="Workout Title" />
 
+                <FieldArray name="steps">
+                    {({ push, remove }) => (
+                        <div>
+                            {values.steps.map((step, index) => (
+                                <div key={step.id}>
+                                    <Field type="text" name={`steps[${index}].exerciseName`} placeholder="Exercise Name" />
+                                    <Field as="select" name={`steps[${index}].interval`}>
+                                        <option value="daily">Daily</option>
+                                        <option value="every2days">Every 2 Days</option>
+                                    </Field>
+
+
+
+
+                                    <button type="button" onClick={() => remove(index)}>Remove Step</button>
+                                    </div>
+                            ))}
+                            <button type="button" onClick={() => push({ id: uuidv4(), exerciseName: '', description: '', sets: '', reps: '', interval: '' })}>Add Step</button>
+                        </div>
+                    )}
+                </FieldArray>
+
                 <button type="submit">Submit</button>
+
+
             </Form>
+
+
             
         )}
         </Formik>
