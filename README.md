b1dff5a26ed262fea0cdfa43d49dab88cbb4e31a
stacks-loops <p.cronin.connolly@gmail.com>
2024-05-02 11:16:38 -0400

    moving tings around

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # glossy-disco
 loops
 
+
+
+
+Frontend: From  frontend directory, use the appropriate start command.
+cd app/frontend
+npm start
+
+Backend: From project root or the app directory, use flask run.
 runtime environment and flask commands should be in mac terminal cd glossy-disco
 from glossy-disco
 flask run
@@ -8,12 +16,5 @@ flask run
 from glossy-dicso
 pipenv shell
 flask shell
-
-
-Frontend: From  frontend directory, use the appropriate start command.
-Backend: From project root or the app directory, use flask run.
-
-cd app/frontend/my-react-app
-npm start
 
 
--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from config import ApplicationConfig
 from models import db, Workout, Step
+from routes import add_workout
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
@@ -8,8 +9,6 @@ from flask_session import Session
 
 from flask_sqlalchemy import SQLAlchemy  # Import for database 
 import ipdb
-
-from models import Workout, Step
 
 app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
@@ -26,31 +25,6 @@ migrate = Migrate(app, db)
 api = Api(app)
 
 CORS(app, origins=['http://localhost:5173'], supports_credentials=True)
-
-
-
-@app.route('/add-workout', methods=['POST'])
-def add_workout():
-    data = request.get_json()
-
-    workout = Workout(title=data['title'])
-
-    for step in data['steps']:
-        new_step = Step(
-            exercise_name=step['exerciseName'],
-            description=step['description'], 
-            sets=step['sets'],
-            reps=step['reps'],
-            interval_type=step['intervalType'],
-            interval_value=step['intervalValue'],
-            workout=workout
-        )
-        db.session.add(new_step)
-
-    db.session.add(workout) 
-    db.session.commit()
-
-    return jsonify({'message': 'Workout created successfully!'}), 201 
 
 if __name__ == '__main__': 
     app.run(debug=True) 
