6d02470250a910187ac245006ee4bc0797f80a06
stacks-loops <p.cronin.connolly@gmail.com>
2024-05-01 22:05:52 -0400

    setting up backend with SQLite and uuid

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import uuid 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///workouts.db'  # Adjust database name/location if needed
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Optional, but generally recommended
+db = SQLAlchemy(app)
